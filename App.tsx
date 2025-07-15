8b44bd27455060d0440b0fb53dafd977b21e9248
YURIKA PRAJAPATI <prajapatiyurika@gmail.com>
2025-07-15 12:40:59 +0545

    created a native stack navigatior

--- App.tsx
+++ App.tsx
@@ -8,26 +8,33 @@
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStaticNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen';
-import TodoScreen from './src/screens/TodoScreen';
+// import HomeScreen from './src/screens/HomeScreen';
+// import TodoScreen from './src/screens/TodoScreen';
 
-const Stack = createNativeStackNavigator(); 
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: HomeScreen,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
+
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+  
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{title: 'Welcome'}}
-        />
-        <Stack.Screen name="Todo" component={TodoScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Navigation />
   );
 }
 
