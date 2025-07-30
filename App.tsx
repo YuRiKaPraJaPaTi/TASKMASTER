f04f97d1d7c83c2746d1b3280056a633c2a934bd
YURIKA PRAJAPATI <prajapatiyurika@gmail.com>
2025-07-30 13:52:38 +0545

    authorize status and fcm token get

--- App.tsx
+++ App.tsx
@@ -16,12 +16,32 @@ import { useAppDispatch, useAppSelector } from './src/redux/hooks';
 import { setTasks } from './src/redux/todoSlice';
 import { RootState } from './src/redux/store';
 import Mainapp from './Mainapp'
+import messaging from '@react-native-firebase/messaging';
 
 // const Stack = createNativeStackNavigator();
 
 function App() {
 
+  async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+  }
+}
+
+const getToken = async()=> {
+  const token = await messaging().getToken()
+  console.log("token: ", token)
+}
+
+useEffect(()=>{
+  requestUserPermission()
+  getToken()
+},[])
   
   return (
     <Provider store={store}>
